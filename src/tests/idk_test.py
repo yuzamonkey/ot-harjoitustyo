339fc12bbd25b4674927f512f4846cf6c835244e
Jusa Mankki <jusamankki@gmail.com>
2021-04-10 11:17:06 +0300

    init testing

---
+++ src/tests/idk_test.py
@@ -0,0 +1,5 @@
+import unittest
+
+class TestIdk(unittest.TestCase):
+  def test_idk(self):
+    self.assertEqual("test", "test")
--- tasks.py
+++ tasks.py
@@ -10,10 +10,11 @@ def test(ctx):
   ctx.run("pytest src")
 
 @task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src")
+
+@task(coverage)
 def coverage_report(ctx):
   print("Make coverage report")
   ctx.run("coverage html")
 
-@task
-def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest src")
