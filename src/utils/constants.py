1513f84ba5c59898e562ee3663b41d338fdf9cb0
Jusa Mankki <jusamankki@gmail.com>
2021-04-24 13:12:40 +0300

    add 6/8 time and note values to constants

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,7 +1,9 @@
 CLEFS = ['G', 'F']
 KEY_SIGNATURES = ['F/d', 'C/a', 'G/e']
-TIME_SIGNATURES = ['2/4', '3/4', '4/4']
-NOTATION_LENGTHS = [2, 4, 8]
+TIME_SIGNATURES = ['2/4', '3/4', '4/4', '6/8']
+NOTATION_LENGTHS = [2, 4, 8, 16]
 PITCHES = [
-  'c4', 'd4', 'e4', 'f4', 'g4', 'a4', 'b4'
+  'c3', 'd3', 'e3', 'f3', 'g3', 'a3', 'b3',
+  'c4', 'd4', 'e4', 'f4', 'g4', 'a4', 'b4',
+  'c5', 'd5', 'e5', 'f5', 'g5', 'a5', 'b5'
 ]
