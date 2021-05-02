ae6227c87d466e8b4ca136d70b2dcf29202acda0
Jusa Mankki <jusamankki@gmail.com>
2021-05-02 13:55:24 +0300

    barlines after notes

--- src/ui/score_view.py
+++ src/ui/score_view.py
@@ -12,7 +12,7 @@ class ScoreView:
 
     self._clef = ImageTk.PhotoImage(Image.open('./src/utils/images/G-clef.gif').resize((120,200)))
     self._time_signature = ImageTk.PhotoImage(Image.open('./src/utils/images/44.gif').resize((70,145)))
-    self._quartet_note = ImageTk.PhotoImage(Image.open('./src/utils/images/Quartet_note.gif').resize((70,145)))
+    self._half_note = ImageTk.PhotoImage(Image.open('./src/utils/images/half_note.gif').resize((90,120)))
 
   def show(self):
     self.update()
@@ -27,32 +27,51 @@ class ScoreView:
 
     if self._title_frame:
       self._title_frame.destroy()
-    self._title_frame = tk.Frame(master=self._frame, height=50, bg='red')
+    self._title_frame = tk.Frame(master=self._frame, height=50)
     self._title_frame.pack(fill=tk.X, side=tk.TOP)
     self._title_frame.pack_propagate(0)
     self._show_title()
 
     if self._score_frame:
       self._score_frame.destroy()
-    self._score_frame = tk.Frame(master=self._frame, bg='green')
+    self._score_frame = tk.Frame(master=self._frame)
     self._score_frame.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
     self._score_frame.columnconfigure([0,1,2], weight=2)
-    self._score_frame.rowconfigure([0,1,2], weight=2)
+    self._score_frame.rowconfigure([0,1], weight=2)
 
     self._show_score()
 
   def _show_title(self):
-    score_title = tk.Label(master=self._title_frame, text=self._score.get_title(), pady=20)
+    score_title = tk.Label(master=self._title_frame, text=self._score.get_title(), pady=20, font=('Arial', 30))
     score_title.pack()
 
   def _show_score(self):
-    measure_count = score_service.get_staff_length()
-    endline = measure_count * 500
+    canvas = tk.Canvas(master=self._score_frame, width=700, height=1000, confine=True, cursor='circle')
 
-    canvas = tk.Canvas(master=self._score_frame, width=700, height=300, confine=True, cursor='circle')
+    measure_count = score_service.get_staff_length()
+    # endline = (measure_count) * 500 - 300
+
+    # Clef
+    canvas.create_image(35, 80, image=self._clef, anchor=tk.constants.NW)
+    # Time signature
+    canvas.create_image(120, 105, image=self._time_signature, anchor=tk.constants.NW)
+
+    notation_position = 200
+    notation_gap = 75
+
+    for i in range (measure_count):
+      measure = self._score.get_staff().get_measures()[i]
+      for notation in measure.get_notations():
+        if (notation.is_note()):
+          canvas.create_image(notation_position, 80, image=self._half_note, anchor=tk.constants.NW)
+          notation_position += notation_gap
+      if i == measure_count-1:
+        break
+      canvas.create_line(notation_position+25, 125, notation_position+25, 225)
+      notation_position += 25
+
+    endline = notation_position + 50
     
-    # Padding top
-    canvas.create_rectangle(0, 0, endline, 50, outline='white')
     # Padding end
     canvas.create_rectangle(endline, 0, endline+50, 100, outline='white')
     # Staff
@@ -65,20 +84,9 @@ class ScoreView:
     canvas.create_line(50, 125, 50, 225, fill='black')
     # Ending diagonal lines
     canvas.create_line(endline-10, 125, endline-10, 225, fill='black')
-    canvas.create_rectangle(endline, 125, endline+10, 225, fill='black')
+    canvas.create_rectangle(endline, 125, endline +10, 225, fill='black')
 
-    # Clef
-    canvas.create_image(35, 80, image=self._clef, anchor=tk.constants.NW)
-    # Time signature
-    canvas.create_image(120, 105, image=self._time_signature, anchor=tk.constants.NW)
 
-    # Bar lines
-    bar_line_position = 100 + endline / measure_count
-    bar_line_width = (endline - 100) / measure_count
-
-    for i in range (measure_count-1):
-      canvas.create_line(bar_line_position, 125, bar_line_position, 225)
-      bar_line_position += bar_line_width
 
 
 
@@ -90,7 +98,7 @@ class ScoreView:
     canvas.configure(xscrollcommand=scroll_x.set)
     canvas.configure(scrollregion=canvas.bbox("all"))
 
-    canvas.grid(row=1, column=0, sticky='nesw', columnspan=3)
+    canvas.grid(row=1, column=0, sticky='esw', columnspan=3)
 
 
     
